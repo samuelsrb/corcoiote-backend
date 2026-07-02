2233d50fd8619ff1b89db277e2b7452ee2c1f7f2
Samuel Soares <ssrbarroso@gmail.com>
2026-07-02 16:42:25 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type { Customer } from "../types.ts";
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: "Cristiano Ronaldo",
+		status: true,
+		email: "cr7@email.com",
+	},
+	{
+		id: 2,
+		name: "Lionel Messi",
+		status: true,
+		email: "leo@email.com",
+	},
+	{
+		id: 3,
+		name: "Neymar Júnior",
+		status: false,
+		email: "ney@email.com",
+	},
+];
